8b322ffd579260e8f22babc70900511fb3857870
qingbuyaozaishuohaha <itxuzhenqin@163.com>
2019-02-25 16:27:01 +0800

    up

--- src/main/java/com/xu/es/demo/controller/EsClientController.java
+++ src/main/java/com/xu/es/demo/controller/EsClientController.java
@@ -3,11 +3,17 @@ package com.xu.es.demo.controller;
 import org.elasticsearch.action.delete.DeleteResponse;
 import org.elasticsearch.action.get.GetResponse;
 import org.elasticsearch.action.index.IndexResponse;
+import org.elasticsearch.action.search.SearchRequestBuilder;
+import org.elasticsearch.action.search.SearchResponse;
+import org.elasticsearch.action.search.SearchType;
 import org.elasticsearch.action.update.UpdateRequest;
 import org.elasticsearch.action.update.UpdateResponse;
 import org.elasticsearch.client.transport.TransportClient;
 import org.elasticsearch.common.xcontent.XContentBuilder;
 import org.elasticsearch.common.xcontent.XContentFactory;
+import org.elasticsearch.index.query.BoolQueryBuilder;
+import org.elasticsearch.index.query.QueryBuilders;
+import org.elasticsearch.index.query.RangeQueryBuilder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.HttpStatus;
@@ -15,8 +21,11 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Date;
-import java.util.concurrent.ExecutionException;
+import java.util.List;
+import java.util.Map;
+
 
 /**
  * es测试连接
@@ -136,6 +145,51 @@ public class EsClientController {
             return new ResponseEntity(HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
+
+    /**
+     * es复杂查询
+     * @param author
+     * @param title
+     * @param gtWordCount
+     * @param ltWordCount
+     * @return
+     */
+    @PostMapping("query/book/novel")
+    @ResponseBody
+    public ResponseEntity query(
+            @RequestParam(name = "author",required = false)String author,
+            @RequestParam(name = "title",required = false)String title,
+            @RequestParam(name = "gt_word_count",defaultValue = "0")String gtWordCount,
+            @RequestParam(name = "lt_word_count",required = false) Integer ltWordCount){
+        BoolQueryBuilder boolQuery = QueryBuilders.boolQuery();
+
+        if (author != null){
+            boolQuery.must(QueryBuilders.matchQuery("author", author));
+        }
+        if (title != null){
+            boolQuery.must(QueryBuilders.matchQuery("title", title));
+        }
+        RangeQueryBuilder rangeQuery = QueryBuilders.rangeQuery("word_count")
+                .from(gtWordCount);
+        if (ltWordCount != null && ltWordCount > 0){
+            rangeQuery.to(ltWordCount);
+        }
+        boolQuery.filter(rangeQuery);
+
+        SearchRequestBuilder builder = this.client.prepareSearch("book")
+                .setTypes("novel")
+                .setSearchType(SearchType.DFS_QUERY_THEN_FETCH)
+                .setQuery(boolQuery)
+                .setFrom(0)
+                .setSize(10);
+        System.out.println(builder);
+
+        SearchResponse response = builder.get();
+        List<Map<String,Object>> result = new ArrayList<>();
+        response.getHits().forEach(hit->result.add(hit.getSourceAsMap()));
+        return new ResponseEntity(result,HttpStatus.OK);
+    }
+
 
 
     @GetMapping("/")
